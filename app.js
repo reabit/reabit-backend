a425ccef34562628b04ee089d58a623af1770bdd
triamri <triamri@gmail.com>
2018-02-07 19:03:32 +0700

    app.js and packege.json

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
-const HttpStatus = require('http-status-codes')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const chatbot = require('./routes/chatbot')
 
-require('dotenv').config()
-mongoose.connection.openUri('mongodb://localhost:27017/movie');
-mongoose.Promise = global.Promise;
-mongoose.connection.once('open', () => {
-  console.log('mongoose connection success');
-}).on('error', (error) => {
-  console.log('connection error', error);
-})
-
+mongoose.connect(`mongodb://triamri:123456@ds229008.mlab.com:29008/readit`)
 const app = express();
-app.use(cors())
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/chatbot', chatbot)
+app.use('/cheerio', require('./routes/cheerio'));
+app.use('/readings', require('./routes/readings'));
+app.use('/summarys', require('./routes/summarys'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
 
 module.exports = app;
